81405cf377be20df3fb979e6c8f9574431d9bbfe
Swibm <swibm9009@gmalil.com>
2023-10-22 03:37:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+﻿var image = document.getElementById("myImage");
+var hideButton = document.getElementById("hideButton");
+
+// Add click event listener to the button
+hideButton.addEventListener("click", function () {
+    // Change the display property to 'none' to hide the image
+    image.style.display = "flex";
+});
+
+var image2 = document.getElementById("myImage2");
+var hideButton2 = document.getElementById("hideButton2");
+
+// Add click event listener to the button
+hideButton2.addEventListener("click", function () {
+    // Change the display property to 'none' to hide the image
+    image2.style.display = "flex";
+});
+
+var image3 = document.getElementById("myImage3");
+var hideButton3 = document.getElementById("hideButton3");
+
+// Add click event listener to the button
+hideButton3.addEventListener("click", function () {
+    // Change the display property to 'none' to hide the image
+    image3.style.display = "flex";
+});
+
+var image4 = document.getElementById("myImage4");
+var hideButton4 = document.getElementById("hideButton4");
+
+// Add click event listener to the button
+hideButton4.addEventListener("click", function () {
+    // Change the display property to 'none' to hide the image
+    image4.style.display = "flex";
+});
+
+var image5 = document.getElementById("myImage5");
+var hideButton5 = document.getElementById("hideButton5");
+
+// Add click event listener to the button
+hideButton5.addEventListener("click", function () {
+    // Change the display property to 'none' to hide the image
+    image5.style.display = "flex";
+});
